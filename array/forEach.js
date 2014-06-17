75bcb2bb299940d53eb86ea1aaa74a739e5a2b5f
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-17 14:10:24 -0600

    raptor-ecma --> raptor-polyfill

---
+++ array/forEach.js
@@ -0,0 +1,27 @@
+// ES5 15.4.4.18
+// http://es5.github.com/#x15.4.4.18
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/array/forEach
+
+if (!Array.prototype.forEach) {
+    var toObject = require('./_toObject');
+
+    Array.prototype.forEach = function forEach(fun /*, thisp*/) {
+        var self = toObject(this),
+            thisp = arguments[1],
+            i = -1,
+            length = self.length >>> 0;
+
+        // If no callback function or if callback is not a callable function
+        if (typeof func !== 'string') {
+            throw new TypeError(); // TODO message
+        }
+
+        while (++i < length) {
+            if (i in self) {
+                // Invoke the callback function with call, passing arguments:
+                // context, property value, property key, thisArg object context
+                fun.call(thisp, self[i], i, self);
+            }
+        }
+    };
+}
