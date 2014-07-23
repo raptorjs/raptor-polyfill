ca141fa230f39aebf491ff57bfc95cd3f65f12d0
Patrick Steele-Idem <pnidem@gmail.com>
2014-07-23 12:24:31 -0600

    #1 incorrect typeof check

--- array/forEach.js
+++ array/forEach.js
@@ -5,22 +5,21 @@
 if (!Array.prototype.forEach) {
     var toObject = require('./_toObject');
 
-    Array.prototype.forEach = function forEach(fun /*, thisp*/) {
-        var self = toObject(this),
-            thisp = arguments[1],
-            i = -1,
-            length = self.length >>> 0;
+    Array.prototype.forEach = function forEach(fun, thisObj) {
+        var self = toObject(this);
+        var i = -1;
+        var length = self.length >>> 0;
 
         // If no callback function or if callback is not a callable function
-        if (typeof func !== 'string') {
-            throw new TypeError(); // TODO message
+        if (typeof func !== 'function') {
+            throw new TypeError();
         }
 
         while (++i < length) {
             if (i in self) {
                 // Invoke the callback function with call, passing arguments:
                 // context, property value, property key, thisArg object context
-                fun.call(thisp, self[i], i, self);
+                fun.call(thisObj, self[i], i, self);
             }
         }
     };
